5efa10f1f100ba1b1420d2250beeca284ee58442
Luciano Alves <luas10c@gmail.com>
2022-10-29 01:30:38 -0300

    feat: server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,53 @@
-import { useEffect, useState } from 'react'
-import { toast } from 'react-hot-toast'
+import axios from 'axios'
+import type { GetServerSideProps } from 'next'
+import { useEffect } from 'react'
 
 import { Candidates } from '../components/candidates'
 
-import { fetchResults } from '../services/results'
+interface Props {
+  candidates: any[]
+}
 
-const Home = () => {
-  const [loading, setLoading] = useState(true)
-  const [candidates, setCandidates] = useState([])
+const Home = (props: Props) => {
+  const { candidates } = props
 
   useEffect(() => {
-    const toaster = toast.loading('Carregando candidatos...')
-    fetchResults()
-      .then((data) => {
-        setCandidates(data)
-      })
-      .finally(() => {
-        setLoading(false)
-        toast.dismiss(toaster)
-      })
+    const interval = setInterval(() => {
+      location.reload()
+    }, 2000)
+
+    return () => {
+      setInterval(interval)
+    }
   }, [])
 
-  if (loading) {
-    return null
-  }
-
   return <Candidates data={candidates} />
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await axios.get(
+    'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json'
+  )
+  const candidates = data.cand.map((item) => {
+    return {
+      id: Number(item.sqcand),
+      coalition: item.cc,
+      party_number: item.n,
+      name: item.nm,
+      name_vice: item.nv,
+      position: Number(item.seq),
+      image_url: Number(item.n) === 22 ? '/bolsonaro.jpg' : '/lula.jpg',
+      pvap: item.pvap,
+      text: item.st,
+      votes: item.vap
+    }
+  })
+
+  return {
+    props: {
+      candidates
+    }
+  }
 }
 
 export default Home
