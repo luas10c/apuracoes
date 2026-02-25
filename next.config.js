5f6819060d5d133d4aa9db8ab76e4d5be43dd492
Luciano Alves <luas10c@gmail.com>
2026-02-25 02:42:25 -0300

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type{import('next').NextConfig} */
 export default {
   turbopack: {
-    root: import.meta.dirname,
+    root: import.meta.dirname
   },
-};
+  images: {
+    qualities: [75, 80]
+  }
+}
