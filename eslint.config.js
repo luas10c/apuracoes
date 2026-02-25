1a9fad3ae4b3dc82d8e1d4f314f94373500fbf4c
Luciano Alves <luas10c@gmail.com>
2026-02-25 01:47:41 -0300

    chore: some changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,54 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-import js from "@eslint/js";
+import globals from 'globals'
 
-import ts from "typescript-eslint";
+import js from '@eslint/js'
 
-import prettier from "eslint-plugin-prettier";
+import ts from 'typescript-eslint'
+
+import prettier from 'eslint-plugin-prettier'
+
+import react from 'eslint-plugin-react'
+import a11y from 'eslint-plugin-jsx-a11y'
+
+import nextjs from '@next/eslint-plugin-next'
 
 export default defineConfig([
-  globalIgnores(["node_modules", ".next"]),
+  globalIgnores(['node_modules', '.next', 'coverage']),
   js.configs.recommended,
   ts.configs.recommended,
   {
     plugins: {
       prettier,
+      react,
+      'jsx-a11y': a11y,
+      '@next/next': nextjs
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.es2022,
+        ...globals.browser,
+        ...globals.node,
+        React: true
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
     rules: {
       ...prettier.configs.recommended.rules,
-    },
-  },
-]);
+      ...react.configs.recommended.rules,
+      ...a11y.configs.recommended.rules,
+      ...nextjs.configs.recommended.rules
+    }
+  }
+])
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type{import('next').NextConfig} */
 export default {
-  //
-}
+  turbopack: {
+    root: import.meta.dirname,
+  },
+};
